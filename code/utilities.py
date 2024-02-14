01dbe984ec22b63d0eb7c12a7e691f6dfbb4fd46
thomas.callister <thomas.a.callister@gmail.com>
2024-02-14 13:57:59 -0600

    Misc updates

--- code/utilities.py
+++ code/utilities.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
+from numpy.random import default_rng
 
 def generalized_Xp(s1x,s1y,s2x,s2y,q):
 
@@ -80,30 +81,47 @@ def load_training_data(
     n_bns = 20000,
     n_nsbh = 20000,
     n_hopeless = 60000,
+    n_certain = 20000,
     rng_key = 111):
 
+    generator = default_rng(rng_key)
+
     # Read injections
-    train_data = pd.read_hdf('{0}/bbh_training_data.hdf'.format(data_directory)).sample(n_bbh,random_state=rng_key)
-    val_data = pd.read_hdf('{0}/bbh_validation_data.hdf'.format(data_directory)).sample(int(n_bbh/4),random_state=rng_key)
-    bns_train_data = pd.read_hdf('{0}/bns_training_data.hdf'.format(data_directory)).sample(n_bns,random_state=rng_key)
-    bns_val_data = pd.read_hdf('{0}/bns_validation_data.hdf'.format(data_directory)).sample(int(n_bns/4),random_state=rng_key)
-    nsbh_train_data = pd.read_hdf('{0}/nsbh_training_data.hdf'.format(data_directory)).sample(n_nsbh,random_state=rng_key)
-    nsbh_val_data = pd.read_hdf('{0}/nsbh_validation_data.hdf'.format(data_directory)).sample(int(n_nsbh/4),random_state=rng_key)
+    train_data = pd.read_hdf('{0}/bbh_training_data.hdf'.format(data_directory)).sample(n_bbh,random_state=generator)
+    val_data = pd.read_hdf('{0}/bbh_validation_data.hdf'.format(data_directory)).sample(int(n_bbh/4),random_state=generator)
+    bns_train_data = pd.read_hdf('{0}/bns_training_data.hdf'.format(data_directory)).sample(n_bns,random_state=generator)
+    bns_val_data = pd.read_hdf('{0}/bns_validation_data.hdf'.format(data_directory)).sample(int(n_bns/4),random_state=generator)
+    nsbh_train_data = pd.read_hdf('{0}/nsbh_training_data.hdf'.format(data_directory)).sample(n_nsbh,random_state=generator)
+    nsbh_val_data = pd.read_hdf('{0}/nsbh_validation_data.hdf'.format(data_directory)).sample(int(n_nsbh/4),random_state=generator)
 
     # Read and split hopeless injections
-    official_hopeless_data = pd.read_hdf('{0}/rpo3-without-hopeless-cut-formatted.hdf'.format(data_directory)).sample(n_hopeless,random_state=rng_key)
-    official_hopeless_data,val_official_hopeless_data = train_test_split(official_hopeless_data,train_size=0.8,random_state=rng_key)
+    official_hopeless_data = pd.read_hdf('{0}/rpo3-without-hopeless-cut-formatted.hdf'.format(data_directory)).sample(n_hopeless,random_state=generator)
+    official_hopeless_data,val_official_hopeless_data = train_test_split(official_hopeless_data,train_size=0.8,random_state=generator.integers(0,high=1024))
+
+    # Read and split certain injections
+    official_certain_data = pd.read_hdf('{0}/rpo3-certain-formatted.hdf'.format(data_directory)).sample(n_certain,random_state=generator)
+    official_certain_data['detected'] = 1
+    official_certain_data['detected'].iloc[official_certain_data.obs_snr<=8] = 0
+
+    # Remove odd events
+    to_clip = (official_certain_data.m1_detector<10.*official_certain_data.luminosity_distance**1.2)
+    official_certain_data['detected'].iloc[to_clip] = 0
+
+    # Split
+    official_certain_data,val_official_certain_data = train_test_split(official_certain_data,train_size=0.8,random_state=generator.integers(0,high=1024))
 
     # Append training and validation sets together and shuffle
     train_data = shuffle(train_data.append(official_hopeless_data).\
+                            append(official_certain_data).\
                             append(bns_train_data).\
                             append(nsbh_train_data),\
-                            random_state=rng_key
+                            random_state=generator.integers(0,high=1024)
                         )
     val_data = shuffle(val_data.append(val_official_hopeless_data).\
+                           append(val_official_certain_data).\
                            append(bns_val_data).\
                            append(nsbh_val_data),
-                           random_state=rng_key
+                           random_state=generator.integers(0,high=1024)
                        )
 
     return train_data,val_data
